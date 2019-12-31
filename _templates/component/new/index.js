d1d282ba5bc4a231f839a4fc423566f226d6a9b2
Chris Garcia <pixelbandito@gmil.com>
2019-12-30 22:33:22 -0600

    Add component folder index file to hygen component template

---
+++ _templates/component/new/index.js
@@ -0,0 +1,6 @@
+---
+to: imports/ui/<%=h.changeCase.pascal(name)%>/index.js
+---
+import <%=h.changeCase.pascal(name)%> from '../<%=h.changeCase.pascal(name)%>'
+
+export default <%=h.changeCase.pascal(name)%>
